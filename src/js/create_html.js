3b8467fbd332ea6b6cef5a6df26fd7f7e518a778
Adam Cooper <frankidyfrank3@Gmail.com>
2021-02-11 23:20:31 -0600

    Added Browser events now the inital lodup is the only thing that floods all data

--- src/js/create_html.js
+++ src/js/create_html.js
@@ -7,6 +7,80 @@ function create_windows(window_list){
     return 
 }
 
+
+function create_tab(tab, html_window){
+
+    // create the elements that make up a "tab"
+    let tab_div = document.createElement("div")
+
+    // fill in the tab data for the "tab"
+    fill_tab_data(tab, tab_div);
+
+    // set the active-tab Attribute to the current tab id if it is active
+    if (tab.active) html_window.setAttribute("active-tab", tab.id);
+
+    // append the tab_div to the html_window
+    html_window.appendChild(tab_div)
+    return
+}
+
+
+
+function update_tab(tab, tab_div_original){
+
+    // create the elements that make up a "tab"
+    let tab_div          = tab_div_original.cloneNode(false)
+    
+    // fill in the tab data for the "tab"
+    fill_tab_data(tab, tab_div);
+
+    if (tab.active) {
+        let window_html         = document.getElementById(`${tab.windowId}`);
+        let prev_active_id      = window_html.getAttribute("active-tab");
+        let prev_active         = document.getElementById(`${prev_active_id}`);
+
+        if (prev_active) {
+            prev_active.classList = "window-tab";
+            window_html.setAttribute("active-tab", tab.id);
+        }
+    }
+
+
+    tab_div_original.parentNode.replaceChild(tab_div, tab_div_original)
+    return
+}
+
+
+
+
+
+function create_empty_window(window){
+    // create the root window element and the window title
+    let html_window          = document.createElement('div');
+    let html_window_title    = document.createElement('div');
+
+    // append the window title to the html window
+    html_window.prepend(html_window_title)
+
+    // set the classes, text and id
+    html_window.classList           = "window"
+    html_window_title.classList     = "window-title"
+    html_window_title.textContent   = `Window #${window.id}`
+    html_window.id                  =  window.id
+
+
+    // add the click listener for opening the window
+    html_window_title.addEventListener("click", ()=>{
+        open_window(window.id)
+    })
+
+    // append the window root to the root
+    root.appendChild(html_window)
+    return
+}
+
+
+
 function create_window(window){
     // create the root window element and the window title
     let html_window          = document.createElement('div');
@@ -20,6 +94,8 @@ function create_window(window){
     html_window_title.classList     = window.focused ? "window-title window-active" : "window-title"
     html_window_title.textContent   = `Window #${window.id}`
     html_window.id                  =  window.id
+
+    if (window.focused) root.setAttribute("active-window", `${window.id}`)
 
     // add the click listener for opening the window
     html_window_title.addEventListener("click", ()=>{
@@ -35,6 +111,9 @@ function create_window(window){
     return
 }
 
+
+
+
 function fill_tab_data(tab, tab_div){
     // determine if a favicon exists or provide one, if the window is audible ignore the favicon in favor of a gif signaling audio playback
     let favicon = tab.favIconUrl ? tab.favIconUrl : "https://icons-for-free.com/iconfiles/png/512/document+files+page+paper+sheet+text+icon-1320168143312091615.png"
@@ -43,7 +122,7 @@ function fill_tab_data(tab, tab_div){
     // create the elements that make up a "tab"
     let img     = document.createElement("img");
     let span    = document.createElement("span");
-    
+
     // set classes for the elements
     img.classList       = "window-favcion";
     span.classList      = "window-title-text";
@@ -54,10 +133,10 @@ function fill_tab_data(tab, tab_div){
     // set the src for the favicon, set the title text for the tab and set the div id
     img.src             = src;
     span.textContent    = tab.title;
-    tab_div.id          = `${tab.windowId}:${tab.id}`;
+    tab_div.id          = `${tab.id}`;
 
 
-    // add a listener for normal click and run a function based on the state of the window 
+    // add a listener for normal click and run a function based on the state of the window
     tab_div.addEventListener("click", (event)=>{
         event.preventDefault();
         chrome.windows.getCurrent({}, (current_window)=>{
@@ -83,42 +162,5 @@ function fill_tab_data(tab, tab_div){
     tab_div.appendChild(img);
     tab_div.appendChild(span);
 
-    return
-}
-
-function create_tab(tab, html_window){
-
-    // create the elements that make up a "tab"
-    let tab_div = document.createElement("div")
-
-    // fill in the tab data for the "tab"
-    fill_tab_data(tab, tab_div);
-
-    // set the active-tab Attribute to the current tab id if it is active
-    if (tab.active) html_window.setAttribute("active-tab", tab.id);
-
-    // append the tab_div to the html_window
-    html_window.appendChild(tab_div)
-    return
-}
-
-function update_tab(tab, tab_div_original){
-
-    // create the elements that make up a "tab"
-    let tab_div          = tab_div_original.cloneNode(false)
-    
-    // fill in the tab data for the "tab"
-    fill_tab_data(tab, tab_div);
-
-    if (tab.active) {
-        let window_html         = document.getElementById(`${tab.windowId}`);
-        let prev_active_id      = window_html.getAttribute("active-tab");
-        let prev_active         = document.getElementById(`${tab.windowId}:${prev_active_id}`);
-        prev_active.classList   = "window-tab";
-        window_html.setAttribute("active-tab", tab.id);
-    }
-
-
-    tab_div_original.parentNode.replaceChild(tab_div, tab_div_original)
     return
 }
--- src/js/tab_events.js
+++ src/js/tab_events.js
@@ -1,6 +1,5 @@
 chrome.tabs.onUpdated.addListener((tab_id, change, tab)=>{
-    console.log(change)
-     let tab_html = document.getElementById(`${tab.windowId}:${tab.id}`)
+     let tab_html = document.getElementById(`${tab.id}`)
     if ((change.status === "complete" || change.audible === false || change.audible || change.title) && tab_html){
         update_tab(tab, tab_html)
     }
@@ -18,10 +17,44 @@ chrome.tabs.onCreated.addListener((tab)=>{
 
 chrome.tabs.onRemoved.addListener((tab_id, { isWindowClosing, windowId })=>{
     let html_window = document.getElementById(`${windowId}`);
-    let tab_html = document.getElementById(`${windowId}:${tab_id}`);
+    let tab_html = document.getElementById(`${tab_id}`);
     if (isWindowClosing){
         html_window.remove();
     } else {
         tab_html.remove();
     }
+})
+
+chrome.tabs.onAttached.addListener((tab_id, {newPosition, newWindowId})=>{
+    let html_window = document.getElementById(`${newWindowId}`);
+    let tab_html = document.getElementById(`${tab_id}`);
+    html_window.insertBefore(tab_html, html_window.children[newPosition === 0 ? 1 : newPosition]);
+})
+
+
+// ==============================================================================
+//                                 Window Events
+// ==============================================================================
+
+
+chrome.windows.onCreated.addListener((window)=>{
+    create_empty_window(window)
+})
+
+chrome.windows.onRemoved.addListener((window_id)=>{
+    let html_window = document.getElementById(`${window_id}`);
+    if (html_window) html_window.remove();
+})
+
+chrome.windows.onFocusChanged.addListener((window_id)=>{
+    if (window_id === -1) return;
+    let prev_active_window      = root.getAttribute("active-window");
+    let prev_active_html_window = document.getElementById(`${prev_active_window}`);
+    let html_window             = document.getElementById(`${window_id}`);
+
+    if (prev_active_html_window) {
+        prev_active_html_window.children[0].classList   = "window-title"
+        html_window.children[0].classList               = "window-title window-active"
+    }
+    root.setAttribute("active-window", `${window_id}`)
 })
