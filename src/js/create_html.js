fa3f4b6638f8060e610b1e2b5a2d5069be19ad94
CooperCooper <52174368+Coop25@users.noreply.github.com>
2021-02-14 13:02:44 -0600

    Dragging and new tab (#9)
    * fix the issues created with the events branch this morning
    
    * Added: dragging of tabs to different locations/windows
    Added: minimizing windows by clicking on the title
    Added: new tab button
    
    Fixed: some tabs retaining the active-tab class

--- src/js/create_html.js
+++ src/js/create_html.js
@@ -1,3 +1,7 @@
+// ==============================================================================
+//                               Window Functions
+// ==============================================================================
+
 function create_windows(window_list){
     // set the root objects html to nothing
     root.innerHTML = ''
@@ -6,6 +10,105 @@ function create_windows(window_list){
     window_list.map(window => create_window(window));
     return 
 }
+
+function create_window(window){
+    // create the root window element and the window title
+    let html_window          = document.createElement('div');
+    let html_window_title    = document.createElement('div');
+    let html_window_new_tab  = document.createElement('div');
+    
+    html_window_new_tab.innerHTML = "&#10133;"
+    html_window_new_tab.classList.add("new-tab-button")
+
+    html_window_new_tab.addEventListener("click", ()=>{
+        create_new_tab(window.id) 
+    })
+
+    // append the window title to the html window
+    html_window.prepend(html_window_new_tab)
+    html_window.prepend(html_window_title)
+
+    
+
+
+    // set the classes, text and id
+    html_window.classList           = "window"
+    html_window_title.classList     = window.focused ? "window-title window-active" : "window-title"
+    html_window_title.textContent   = `Window #${window.id}`
+    html_window.id                  =  window.id
+    html_window.setAttribute("is-hidden", "false")
+    html_window.setAttribute("needs-to-hide", "false")
+
+
+    // add the click listener for opening the window
+    html_window_title.addEventListener("click", ()=>{
+        // open_window(window.id)
+        if (html_window.getAttribute("is-hidden") === "true"){
+            show_window(html_window)
+        } else {
+            hide_window(html_window)
+        }
+    })
+
+    html_window.addEventListener('dragover', e => {
+        e.preventDefault()
+
+        if (html_window.getAttribute("is-hidden") === "true") {
+            html_window.setAttribute("needs-to-hide", "true")
+            show_window(html_window)
+        }
+
+        const afterElement = getDragAfterElement(html_window, e.clientY)
+        const draggable = document.querySelector('.dragging')
+        if (afterElement == null) {
+            html_window.appendChild(draggable)
+        } else {
+            html_window.insertBefore(draggable, afterElement)
+        }
+    })
+
+    // loop through the window's tabs
+    if(window.tabs?.length > 0) window.tabs.map(tab => create_tab(tab, html_window));
+
+
+    // append the window root to the root
+    root.appendChild(html_window)
+    return
+}
+
+function getDragAfterElement(window_html, y) {
+    const draggableElements = [...window_html.querySelectorAll('.draggable:not(.dragging)')]
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child }
+        } else {
+            return closest
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+}
+
+function show_window(html_window){
+    html_window.setAttribute("is-hidden", "false")
+        Array.from(html_window.children).forEach(el=>{
+        if(el.classList.contains("hidden") && el.classList.contains("window-tab")) el.classList.remove("hidden")
+    })
+}
+
+function hide_window(html_window){
+    html_window.setAttribute("is-hidden", "true")
+    Array.from(html_window.children).forEach(el=>{
+        if(!el.classList.contains("hidden") && el.classList.contains("window-tab")) el.classList.add("hidden")
+    })
+}
+
+
+
+// ==============================================================================
+//                               Tab Functions
+// ==============================================================================
 
 
 function create_tab(tab, html_window){
@@ -16,103 +119,36 @@ function create_tab(tab, html_window){
     // fill in the tab data for the "tab"
     fill_tab_data(tab, tab_div);
 
-    // set the active-tab Attribute to the current tab id if it is active
-    if (tab.active) html_window.setAttribute("active-tab", tab.id);
+    
+    if (html_window.getAttribute("is-hidden") === "true"){
+        tab_div.classList.add('hidden')
+    }
 
     // append the tab_div to the html_window
     html_window.appendChild(tab_div)
     return
 }
 
-
-
 function update_tab(tab, tab_div_original){
 
     // create the elements that make up a "tab"
     let tab_div          = tab_div_original.cloneNode(false)
-    
-    // fill in the tab data for the "tab"
-    fill_tab_data(tab, tab_div);
 
     if (tab.active) {
         let window_html         = document.getElementById(`${tab.windowId}`);
-        let prev_active_id      = window_html.getAttribute("active-tab");
-        let prev_active         = document.getElementById(`${prev_active_id}`);
-
-        if (prev_active) {
-            prev_active.classList = "window-tab";
-            window_html.setAttribute("active-tab", tab.id);
-        }
+        tab_active_switch(window_html)
     }
 
+    // fill in the tab data for the "tab"
+    fill_tab_data(tab, tab_div);
+
+    if (tab_div_original.parentNode.getAttribute("is-hidden") === "true"){
+        tab_div.classList.add('hidden')
+    }
 
     tab_div_original.parentNode.replaceChild(tab_div, tab_div_original)
     return
 }
-
-
-
-
-
-function create_empty_window(window){
-    // create the root window element and the window title
-    let html_window          = document.createElement('div');
-    let html_window_title    = document.createElement('div');
-
-    // append the window title to the html window
-    html_window.prepend(html_window_title)
-
-    // set the classes, text and id
-    html_window.classList           = "window"
-    html_window_title.classList     = "window-title"
-    html_window_title.textContent   = `Window #${window.id}`
-    html_window.id                  =  window.id
-
-
-    // add the click listener for opening the window
-    html_window_title.addEventListener("click", ()=>{
-        open_window(window.id)
-    })
-
-    // append the window root to the root
-    root.appendChild(html_window)
-    return
-}
-
-
-
-function create_window(window){
-    // create the root window element and the window title
-    let html_window          = document.createElement('div');
-    let html_window_title    = document.createElement('div');
-
-    // append the window title to the html window
-    html_window.appendChild(html_window_title)
-
-    // set the classes, text and id
-    html_window.classList           = "window"
-    html_window_title.classList     = window.focused ? "window-title window-active" : "window-title"
-    html_window_title.textContent   = `Window #${window.id}`
-    html_window.id                  =  window.id
-
-    if (window.focused) root.setAttribute("active-window", `${window.id}`)
-
-    // add the click listener for opening the window
-    html_window_title.addEventListener("click", ()=>{
-        open_window(window.id)
-    })
-
-    // loop through the window's tabs
-    window.tabs.map(tab => create_tab(tab, html_window));
-
-
-    // append the window root to the root
-    root.appendChild(html_window)
-    return
-}
-
-
-
 
 function fill_tab_data(tab, tab_div){
     // determine if a favicon exists or provide one, if the window is audible ignore the favicon in favor of a gif signaling audio playback
@@ -126,7 +162,8 @@ function fill_tab_data(tab, tab_div){
     // set classes for the elements
     img.classList       = "window-favcion";
     span.classList      = "window-title-text";
-    tab_div.classList   = tab.active ? "window-tab tab-active" : "window-tab";
+    tab_div.classList   = tab.active ? "window-tab draggable tab-active" : "window-tab draggable";
+    tab_div.setAttribute("draggable","true")
 
 
 
@@ -156,6 +193,22 @@ function fill_tab_data(tab, tab_div){
             remove_tab(tab.id)
         }
     });
+
+    tab_div.addEventListener('dragstart', () => {
+        tab_div.classList.add('dragging')
+    })
+
+    tab_div.addEventListener('dragend', () => {
+        tab_div.classList.remove('dragging')
+        let index = [...tab_div.parentNode.children].indexOf(tab_div) - 2
+        let window_id = parseInt(tab_div.parentNode.id);
+        let window_html = tab_div.parentNode
+        move_tab_window(tab.id, index, window_id);
+        if (window_html.getAttribute("needs-to-hide") === "true") {
+            window_html.setAttribute("needs-to-hide", "false")
+            hide_window(window_html)
+        }
+    })
 
 
     // append the favicon and text to the tab_div
